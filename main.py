f0407f21448dd4b70445cce41d52a03b647ba1aa
KenToukou <iamkentoko1050@gmail.com>
2024-07-06 01:00:38 +0900

    add gitignore

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 """ ペリーで実行する際のファイル """
+
+import pandas as pd
+
+from schemas import FolderPathModel
+
+folder_model = FolderPathModel
+test_df = pd.read_csv(folder_model.base_data_path / "train.csv")
+print(test_df.shape)
